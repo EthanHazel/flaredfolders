d9bb1f06089dc223d7cbada737159459ca18a8db
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-03-06 18:29:35 -0500

    update(set-icon): added checks for valid slugs on icon libs

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ This project is currently WIP
 
 ## Current Bugs
 
-| Action                       | Causes                                                                           | Desired Output                          |
-| ---------------------------- | -------------------------------------------------------------------------------- | --------------------------------------- |
-| Generating folders too fast  | Bugged folders rendered with differing colors and multiple icons                 | Normal generation                       |
-| Loading into site on refresh | Icon set to no icon even if it is checked as something else by the radio buttons | Set radio buttons to no icon by default |
-| Loading a custom image       | Crops the image to be a 1x1 aspect ratio                                         | Keep the original aspect ratio          |
+| Action                      | Causes                                                           | Desired Output                 |
+| --------------------------- | ---------------------------------------------------------------- | ------------------------------ |
+| Generating folders too fast | Bugged folders rendered with differing colors and multiple icons | Normal generation              |
+| Loading a custom image      | Crops the image to be a 1x1 aspect ratio                         | Keep the original aspect ratio |
--- js/icon-modal.js
+++ js/icon-modal.js
@@ -6,6 +6,9 @@ const REAL_ICON = document.getElementById("real-icon");
 const LUCIDE_SLUG = document.getElementById("lucide-slug");
 const SIMPLE_SLUG = document.getElementById("simple-slug");
 
+const LUCIDE_LINK = document.getElementById("lucide-link");
+const SIMPLE_LINK = document.getElementById("simple-link");
+
 const ICON_RADIOS = document.querySelectorAll('input[name="icon"]');
 
 const NO_ICON = 0;
@@ -25,12 +28,60 @@ let currentSimpleSlug = null;
 let currentSimpleImage = null;
 let currentLucideImage = null;
 
+const updateInputs = () => {
+  if (ICON_RADIOS[UPLOAD_ICON].checked) {
+    LUCIDE_SLUG.classList.add("hidden");
+    SIMPLE_SLUG.classList.add("hidden");
+    LUCIDE_LINK.classList.add("hidden");
+    SIMPLE_LINK.classList.add("hidden");
+    ICON_UPLOAD.classList.remove("hidden");
+  } else if (ICON_RADIOS[LUCIDE_ICON].checked) {
+    LUCIDE_SLUG.classList.remove("hidden");
+    SIMPLE_SLUG.classList.add("hidden");
+    LUCIDE_LINK.classList.remove("hidden");
+    SIMPLE_LINK.classList.add("hidden");
+    ICON_UPLOAD.classList.add("hidden");
+  } else if (ICON_RADIOS[SIMPLE_ICON].checked) {
+    LUCIDE_SLUG.classList.add("hidden");
+    SIMPLE_SLUG.classList.remove("hidden");
+    LUCIDE_LINK.classList.add("hidden");
+    SIMPLE_LINK.classList.remove("hidden");
+    ICON_UPLOAD.classList.add("hidden");
+  } else {
+    LUCIDE_SLUG.classList.add("hidden");
+    SIMPLE_SLUG.classList.add("hidden");
+    LUCIDE_LINK.classList.add("hidden");
+    SIMPLE_LINK.classList.add("hidden");
+    ICON_UPLOAD.classList.add("hidden");
+  }
+};
+
 const fetchSimpleIcon = async (slug) => {
-  return `https://cdn.jsdelivr.net/npm/simple-icons@v14/icons/${slug}.svg`;
+  return `https://simpleicons.org/icons/${slug}.svg`;
+};
+
+const checkValidSimpleSlug = async (slug) => {
+  const url = `https://simpleicons.org/icons/${slug}.svg`;
+  const response = await fetch(url);
+  if (response.ok) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const fetchLucideIcon = async (slug) => {
   return `https://unpkg.com/lucide-static@latest/icons/${slug}.svg`;
+};
+
+const checkValidLucideSlug = async (slug) => {
+  const url = `https://unpkg.com/lucide-static@latest/icons/${slug}.svg`;
+  const response = await fetch(url);
+  if (response.ok) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const openModal = () => {
@@ -54,16 +105,28 @@ const cancelModal = () => {
   return;
 };
 
-const saveModal = () => {
+const saveModal = async () => {
   currentMode = Array.from(ICON_RADIOS).findIndex((radio) => radio.checked);
   if (currentMode == UPLOAD_ICON) {
     REAL_ICON.files = ICON_UPLOAD.files;
   }
   if (currentMode == SIMPLE_ICON) {
+    const slugCheck = await checkValidSimpleSlug(SIMPLE_SLUG.value);
+    if (slugCheck == false) {
+      alert("Invalid Simple slug!");
+      SIMPLE_SLUG.value = prevSimpleSlug;
+      return;
+    }
     currentSimpleSlug = SIMPLE_SLUG.value;
     currentSimpleImage = fetchSimpleIcon(currentSimpleSlug);
   }
   if (currentMode == LUCIDE_ICON) {
+    const slugCheck = await checkValidLucideSlug(LUCIDE_SLUG.value);
+    if (slugCheck == false) {
+      alert("Invalid Lucide slug!");
+      LUCIDE_SLUG.value = prevLucideSlug;
+      return;
+    }
     currentLucideSlug = LUCIDE_SLUG.value;
     currentLucideImage = fetchLucideIcon(currentLucideSlug);
   }
@@ -72,3 +135,7 @@ const saveModal = () => {
   disabledHandler();
   return;
 };
+
+ICON_RADIOS.forEach((radio) => radio.addEventListener("change", updateInputs));
+
+ICON_RADIOS[0].checked = true;
