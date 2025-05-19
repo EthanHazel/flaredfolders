029d47595d242d39cbfa0b353b8bda891cc28415
Ethan Hazel <ethanyvonhazel@gmail.com>
2025-05-19 17:16:25 -0400

    update(not-found): fixed missing root

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,21 +4,28 @@ import Link from "next/link";
 import "@/styles/globals.css";
 import "@/styles/not-found.css";
 
-export default async function NotFound() {
+export default async function RootLayout({ children }) {
   const cookiesInstance = await cookies();
   const theme = cookiesInstance.get("theme")?.value || "light";
+
   return (
     <html lang="en">
-      <body className={theme === "dark" ? "not-found dark" : "not-found"}>
-        <div className="not-found-text">
-          <h1>404</h1>
-          <h2>Page Not Found</h2>
-          <p>Not sure how you got here lol.</p>
-        </div>
-        <div>
-          <Link href="/">Go back to Home</Link>
-        </div>
-      </body>
+      <body className={theme === "dark" ? "dark" : ""}>{children}</body>
     </html>
+  );
+}
+
+export async function NotFound() {
+  return (
+    <RootLayout>
+      <div className="not-found-text">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>Not sure how you got here lol.</p>
+      </div>
+      <div>
+        <Link href="/">Go back to Home</Link>
+      </div>
+    </RootLayout>
   );
 }
