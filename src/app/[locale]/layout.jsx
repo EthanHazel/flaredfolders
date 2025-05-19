78ccc8d39e27c94d1abd3c79522599e88bdbc6d1
Ethan Hazel <ethanyvonhazel@gmail.com>
2025-05-19 14:31:17 -0400

    add(meta): created a meta banner among other changes

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -4,10 +4,15 @@ import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
 import { hasLocale } from "next-intl";
 
+const description =
+  "Generate custom folder designs for your favorite operating system. Free, open source, and no ads. Import your own icons, use Simple Icons, or use Lucide icons.";
+
+const title = "Flared Folders";
+const url = "https://flaredfolders.vercel.app/";
+
 export const metadata = {
-  title: "Flared Folders",
-  description:
-    "Generate custom folders for Windows 11 and Windows 10. Free, open source, and no ads. Import your own icons, use Simple Icons, or use Lucide icons.",
+  title,
+  description,
   links: [
     { rel: "icon", type: "image/png", href: "/favicon/96.png", sizes: "96x96" },
     { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
@@ -15,7 +20,45 @@ export const metadata = {
     { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple.png" },
     { rel: "manifest", href: "/favicon/site.webmanifest" },
   ],
-  appleMobileWebAppTitle: "Flared Folders",
+  openGraph: {
+    title,
+    description,
+    images: [
+      {
+        url: "/meta/banner.png",
+        alt: "Flared Folders Banner",
+        width: 1200,
+        height: 628,
+      },
+      {
+        url: "/favicon/96.png",
+        width: 96,
+        height: 96,
+        alt: "Flared Folders",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    images: [
+      {
+        url: "/meta/banner.png",
+        alt: "Flared Folders Banner",
+        width: 1200,
+        height: 628,
+      },
+      {
+        url: "/favicon/96.png",
+        width: 96,
+        height: 96,
+        alt: "Flared Folders",
+      },
+    ],
+  },
+  appleMobileWebAppTitle: title,
+  url,
 };
 
 export default async function RootLayout({ children, params }) {
