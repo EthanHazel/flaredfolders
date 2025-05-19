46c511c3a50b2f7364cd2ecad759a65e13ab8dc1
Ethan Hazel <ethanyvonhazel@gmail.com>
2025-05-19 17:31:16 -0400

    update(package): installed analytics

--- src/app/[...not_found]/page.jsx
+++ src/app/[...not_found]/page.jsx
@@ -1,5 +1,23 @@
-import { notFound } from "next/navigation";
+import Link from "next/link";
+
+import "@/styles/globals.css";
+import "@/styles/not-found.css";
 
 export default function NotFoundCatchAll() {
-  notFound();
+  return (
+    <html lang="en">
+      <body className="dark">
+        <div className="not-found">
+          <div className="not-found-text">
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>Not sure how you got here lol.</p>
+          </div>
+          <div>
+            <Link href="/">Go back to Home</Link>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
