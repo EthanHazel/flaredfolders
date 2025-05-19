0df504320e82b81c6f8309497e359bb6796de22c
Ethan Hazel <ethanyvonhazel@gmail.com>
2025-05-19 17:14:58 -0400

    update(not-found): fixed missing root

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,15 +8,17 @@ export default async function NotFound() {
   const cookiesInstance = await cookies();
   const theme = cookiesInstance.get("theme")?.value || "light";
   return (
-    <body className={theme === "dark" ? "not-found dark" : "not-found"}>
-      <div className="not-found-text">
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
-        <p>Not sure how you got here lol.</p>
-      </div>
-      <div>
-        <Link href="/">Go back to Home</Link>
-      </div>
-    </body>
+    <html lang="en">
+      <body className={theme === "dark" ? "not-found dark" : "not-found"}>
+        <div className="not-found-text">
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
+          <p>Not sure how you got here lol.</p>
+        </div>
+        <div>
+          <Link href="/">Go back to Home</Link>
+        </div>
+      </body>
+    </html>
   );
 }
