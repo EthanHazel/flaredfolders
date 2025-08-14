80d598290d0db0a296a7dd1fa6ba8f7ed7b6696a
Ethan Hazel <ethanyvonhazel@gmail.com>
2025-08-14 16:13:47 -0400

    Updated screen notice to be better looking

--- src/components/layout/view.jsx
+++ src/components/layout/view.jsx
@@ -1,19 +1,15 @@
-import Image from "next/image";
+import { Expand } from "lucide-react";
 
 import "@/styles/layout/view.css";
 
 export default function ViewLayout() {
   return (
     <div id="mobile">
-      <Image
-        src="/favicon/96.png"
-        alt="Flared Folders"
-        width={96}
-        height={96}
-      />
-      <h1>Flared Folders</h1>
-      <h3>A Free, Open-Source Folder Customization Tool</h3>
-      <p>Please visit on a larger screen in order to use this tool.</p>
+      <Expand />
+      <p>
+        <b>Please visit on a larger screen in order to use this tool.</b>
+      </p>
+      <p>Minimum resolution: 980x500</p>
     </div>
   );
 }
