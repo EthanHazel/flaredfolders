2fd79d96a598a39b2b54e8b2e2379a534f09b396
Ethan Hazel <ethanyvonhazel@gmail.com>
2025-08-19 15:06:24 -0400

    New default icon for lucide

--- src/stores/folder-config.jsx
+++ src/stores/folder-config.jsx
@@ -23,7 +23,7 @@ export const folderConfigStore = create((set) => ({
   iconType: "lucide",
   setIconType: (type) => set({ iconType: type }),
 
-  lucideSlug: "ImageUp",
+  lucideSlug: "Import",
   setLucideSlug: (slug) => set({ lucideSlug: slug }),
   lucideStrokeWidth: 1.5,
   setLucideStrokeWidth: (width) => set({ lucideStrokeWidth: width }),
